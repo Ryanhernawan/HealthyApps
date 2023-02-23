5fad7508d69d5a57e30eea1cca70bd2ba8bf8a4f
Ryanhernawan <ryan.hernawan1508@gmail.com>
2023-02-23 13:47:58 +0700

    adding method login sign up

--- Healthy Apps/Healthy Apps/Controllers/HealthyAppsController.cs
+++ Healthy Apps/Healthy Apps/Controllers/HealthyAppsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Healthy_Apps.Model.Request;
+using Healthy_Apps.Model.Response;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Healthy_Apps.Controllers
@@ -7,6 +9,92 @@ namespace Healthy_Apps.Controllers
     [ApiController]
     public class HealthyAppsController : ControllerBase
     {
+        #region Halaman Pertama
+        [HttpPost]
+        [ActionName("UserSignUp")]
+        public UserSignUpResponse UserSignup(UserSignUpRequest request)
+        {
+            UserSignUpResponse response = new UserSignUpResponse();
+            HealthyAppsProc proc = new HealthyAppsProc();
+            string[] body =
+            {
+                "Username: " + request.Username,
+                "Email: " + request.Email,
+                "Password: " + request.Password
+            };
+            try
+            {
+                if(request.Username == null || request.Username == "")
+                {
+                    response.Message = "Username Tidak Boleh Kosong";
+                    response.MessageCode = 400;
+                }
+                else if(request.Email == null || request.Email == "")
+                {
+                    response.Message = "Email Tidak Boleh Kosong";
+                    response.MessageCode = 400;
+                }
+                else if(request.Password == null)
+                {
+                    response.Message = "Password Tidak Boleh Kosong";
+                    response.MessageCode = 400;
+                }
+                else if(request.Password.Length >= 16)
+                {
+                    response.Message = "Password anda terlalu panjang";
+                    response.MessageCode = 400;
+                }
+                else
+                {
+                    response.data = proc.UserSignUp(request);
+                    response.Message = "Success Login";
+                    response.MessageCode = 200;
+                }
+            }
+            catch
+            {
+                response.Message = "Gagal Login";
+                response.MessageCode = 400;
+            }
+            return response;
+        }
+
+        [HttpPost]
+        [ActionName("UserLogin")]
+        public UserLoginResponse UserLogin(UserLoginRequest request)
+        {
+            UserLoginResponse response = new UserLoginResponse();
+            HealthyAppsProc proc = new HealthyAppsProc();
+
+            string[] body =
+            {
+                "Email: " + request.Email,
+                "Password: " + request.Password
+            };
+
+            try
+            {
+                response.data = proc.UserLogin(request);
+                response.Message = "Success";
+                response.MessageCode = 200;
+            }
+            catch
+            {
+                response.Message = "Failed";
+                response.MessageCode = 400;
+            }
+
+            return response;
+        }
+        //[HttpPost]
+        //[ActionName("CheckUsedEmail")]
+
+
+        
+        //[HttpPost]
+        //[ActionName("CheckUsedUsername")]
+
+        #endregion
 
     }
 }
--- Healthy Apps/Healthy Apps/HealthyAppsProc.cs
+++ Healthy Apps/Healthy Apps/HealthyAppsProc.cs
@@ -1,8 +1,82 @@
-﻿namespace Healthy_Apps
+﻿using BCrypt.Net;
+using Healthy_Apps.Model;
+using Healthy_Apps.Model.Request;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Healthy_Apps
 {
     public class HealthyAppsProc
     {
         public string HealhtyApps = Startup.connectionString["connectionString:Connect"];
 
+        #region HALAMAN LOGIN SIGN UP
+        public DataTable UserSignUp(UserSignUpRequest input)
+        {
+            DataTable dt = new DataTable();
+            string query = "sp_SignUp";
+            SqlConnection connection = new SqlConnection();
+            try
+            {
+                connection.ConnectionString = HealhtyApps; 
+                SqlCommand command = new SqlCommand(query, connection); 
+                command.CommandType = CommandType.StoredProcedure;
+
+
+                command.Parameters.Add(new SqlParameter("Username", input.Username));
+                command.Parameters.Add(new SqlParameter("Email", input.Email));
+                command.Parameters.Add(new SqlParameter("Password", input.Password));
+
+                connection.Open();
+                var dataReader = command.ExecuteReader();
+                dt.Load(dataReader);
+                return dt;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }            
+        }
+
+        public DataTable UserLogin(UserLoginRequest input)
+        {
+            DataTable dt = new DataTable();
+            string query = "sp_Login";
+            SqlConnection connection = new SqlConnection();
+            try
+            {
+                connection.ConnectionString = HealhtyApps;
+                SqlCommand command = new SqlCommand(query, connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.Add(new SqlParameter("Email", input.Email));
+                command.Parameters.Add(new SqlParameter("Password", input.Password));
+
+                connection.Open();
+                var dataReader = command.ExecuteReader();
+                dt.Load(dataReader);
+                return dt;
+
+
+
+
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        #endregion
     }
 }
