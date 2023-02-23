77013f194ca96f5939d08f34874bc30c88379882
Ryanhernawan <ryan.hernawan1508@gmail.com>
2023-02-23 12:38:58 +0700

    Commit Login Signup Request

---
+++ Healthy Apps/Healthy Apps/Model/Request/UserSignUpRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Healthy_Apps.Model.Request
+{
+    public class UserSignUpRequest
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
